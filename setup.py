235391ea42127572cc04961409c407d6b580a5e0
Paul Tagliamonte <tag@pault.ag>
2015-09-15 11:19:10 -0400

    Add in setup.py; gitignore

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name="PJD5132",
+    version="0.1",
+    packages=['PJD5132',],
+    author="Paul Tagliamonte",
+    author_email="paultag@gmail.com",
+    long_description=long_description,
+    description='n/a',
+    license="GPL-3.0+",
+    install_requires=["hy",],
+    url="",
+    platforms=['any']
+)
